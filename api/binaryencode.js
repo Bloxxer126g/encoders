d9a4d46ac143e940a4f9c6cf1ea9f516c2bd0e5e
Bloxxer126g <tristan.raneroyay@proton.me>
2026-06-20 18:19:54 +0100

    fixit

--- api/binaryencode.js
+++ api/binaryencode.js
@@ -1,15 +1,14 @@
-export default function(req, res) {
-    try {
-        text = req.body;
+function BinaryEncode(text) {
+    res.body = text.split('').map(char => {
+        return char.charCodeAt(0).toString(2).padStart(8, '0');
+    }).join(' ');
+}
 
-        res.body = text.split('').map(char => {
-            return char.charCodeAt(0).toString(2).padStart(8, '0');
-        }).join(' ');
-    
-        res.statuscode = 200;
-    } catch {
-        res.statuscode = 500;
+export default async function handler(req, res) {
+    try {
+        const data = await BinaryEncode(req.body);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message || String(error) });
     }
-    
-    return {res}
 }
